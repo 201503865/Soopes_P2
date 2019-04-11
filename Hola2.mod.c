bad01cd1a0f0807210cff9283a3cffafdbe3699c
DanielGarcia097 <42197600+DanielGarcia097@users.noreply.github.com>
2019-04-11 03:05:00 -0600

    Modulo2

---
+++ Hola2.mod.c
@@ -0,0 +1,48 @@
+#include <linux/build-salt.h>
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+BUILD_SALT;
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+#ifdef CONFIG_RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+static const struct modversion_info ____versions[]
+__used
+__attribute__((section("__versions"))) = {
+	{ 0x83514545, "module_layout" },
+	{ 0xe97c2c8b, "remove_proc_entry" },
+	{ 0xef0dcb5, "proc_create" },
+	{ 0x37a0cba, "kfree" },
+	{ 0xc5850110, "printk" },
+	{ 0xdecd0b29, "__stack_chk_fail" },
+	{ 0xe914e41e, "strcpy" },
+	{ 0xffcbaa56, "init_task" },
+	{ 0x8950eda2, "kmem_cache_alloc_trace" },
+	{ 0xd6ddb0ce, "kmalloc_caches" },
+	{ 0xb44ad4b3, "_copy_to_user" },
+	{ 0x88db9f48, "__check_object_size" },
+	{ 0x754d539c, "strlen" },
+	{ 0xbdfb6dbb, "__fentry__" },
+};
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=";
+
